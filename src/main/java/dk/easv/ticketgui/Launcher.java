d8aed4acdae7e73753d4ab2f2fb1db3c48d24929
Zilas Larsen <zilas04@gmail.com>
2026-02-03 12:10:16 +0100

    Nyere version

---
+++ src/main/java/dk/easv/ticketgui/Launcher.java
@@ -0,0 +1,9 @@
+package dk.easv.ticketgui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/dk/easv/ticketgui/LoginController.java
@@ -0,0 +1,13 @@
+package dk.easv.ticketgui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+
+    @FXML
+    private void onSignInClick(ActionEvent actionEvent) {
+    }
+}
