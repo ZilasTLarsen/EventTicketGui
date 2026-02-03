b0270cdd8aeb623d7516ba4f3984d12071c78420
Frederik Ehmsen <Freehm01@easv365.dk>
2026-02-03 13:46:45 +0100

    made some changes on coordinator controller and login function

---
+++ src/main/java/dk/easv/ticketgui/CoordinatorController.java
@@ -0,0 +1,13 @@
+package dk.easv.ticketgui;
+
+import io.github.palexdev.mfxcore.controls.Label;
+import javafx.fxml.FXML;
+
+public class CoordinatorController {
+    @FXML
+    private Label lblTotalEvents;
+    @FXML
+    private Label lblUpcomingEvents;
+    @FXML
+    private Label lblTotalTickets;
+}
